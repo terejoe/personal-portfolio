f82e46ba9e38237b27b591f2357d685d4c165f38
terejoe <nzeamalunkechinyere@gmail.com>
2022-10-10 13:53:28 +0100

    fixed margin bug in the project section(2)

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -5,7 +5,7 @@ import budget from '../assets/budget.png'
 
 export default function Project() {
   return (
-    <div name ='projects' className='w-full pt-2 md:h-screen bg-[#010021] text-gray-300'>
+    <div name ='projects' className='w-full pt-4 md:h-screen bg-[#010021] text-gray-300'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-6'>
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-green-600'>Projects</p>
