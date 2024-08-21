34e2770a4d1392eab1d225452233f1c749cf217d
terejoe <nzeamalunkechinyere@gmail.com>
2024-08-21 12:41:17 +0100

    resolved card gaps

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -4,15 +4,15 @@ import budget from '../assets/budget.png'
 
 export default function Project() {
   return (
-    <div name ='projects' className='w-full pt-40 bg-[#010021] text-gray-300'>
+    <div name ='projects' className='w-full pt-20 md:pt-40 bg-[#010021] text-gray-300'>
       <div className='max-w-[1000px] md:pl-40 p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-6'>
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-green-600'>Projects</p>
         </div>
 
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4 mx-auto'>
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 md:gap-72 gap-10 mx-auto'>
           {/* One Item */}
-          <div className='border-lg shadow-lg shadow-black container rounded-md justify-center items-center max-w-sm overflow-hidden'>
+          <div className='border-lg shadow-lg shadow-black container rounded-md justify-center items-center md:w-[350px] overflow-hidden'>
             <a href=' https://tickety-group10.netlify.app/'><img src={tickety} alt='Website' className='w-full'/></a>
             <div className='px-6 py-4'>
               <p className='font-bold text-gray-500 text-xl mb-2'>Event Mangement System</p>
@@ -23,7 +23,7 @@ export default function Project() {
             </div>
           </div>
 
-          <div className='border-lg shadow-lg shadow-black container rounded-md justify-center items-center max-w-sm overflow-hidden'>
+          <div className='border-lg shadow-lg shadow-black container rounded-md justify-center items-center md:w-[350px] overflow-hidden'>
             <a href='https://instagram-react-app.netlify.app/'><img src={instagram} alt='Website' className='w-full'/></a>
             <div className='px-6 py-4'>
               <p className='font-bold text-gray-500 text-xl mb-2'>Instagram App Clone</p>
@@ -34,7 +34,7 @@ export default function Project() {
             </div>
           </div>
 
-          <div className='border-lg shadow-lg shadow-black container rounded-md justify-center items-center max-w-sm overflow-hidden'>
+          <div className='border-lg shadow-lg shadow-black container rounded-md justify-center items-center md:w-[350px] overflow-hidden'>
             <a href='https://budget-and-expenses-applicatin.netlify.app/'><img src={budget} alt='Website' className='w-full'/></a>
             <div className='px-6 py-4'>
               <p className='font-bold text-gray-500 text-xl mb-2'>Budget App</p>
